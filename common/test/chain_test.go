0f020e94e29672526486ee587bb295561ad5edc1
ss <17279242@qq.com>
2018-01-03 09:36:58 +0800

    调整协议类工程目录,从agent工程中转移到common工程中

--- common/test/chain_test.go
+++ common/test/chain_test.go
@@ -4,12 +4,36 @@ import (
 	"testing"
 	"github.com/zhifeichen/bluesky-protocol/common/chains"
 	"fmt"
+	"time"
 )
+
+
+type PrintItem struct {
+	chains.BaseItem
+	say string
+}
+
+func NewPrintItem(name,say string) *PrintItem{
+	return &PrintItem{
+		BaseItem:BaseItem{
+			name:name,
+		},
+	}
+}
+
+func (item *PrintItem) Do(data interface{}) (error,interface{}){
+	fmt.Println(item.GetName()," say:",item.say);
+	return nil,data
+}
+
 
 func TestChain(t *testing.T) {
 	t.Run("测试 chain", func(t *testing.T) {
 		lineChain := chains.NewLineChains("测试")
 		fmt.Println(lineChain.String())
 		lineChain.Run()
+
+		time.Sleep(time.Duration(1)*time.Second)
+		//lineChain.S
 	})
 }
